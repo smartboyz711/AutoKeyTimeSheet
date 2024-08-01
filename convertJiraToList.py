e1b1ba1e4526f94679fd58dcbb1ec4e2894749b5
AWARE\theedanai.p <theedanai.p@aware.co.th>
2024-08-01 12:07:58 +0700

    refactor code determine_task , determine_bill_type

--- convertJiraToList.py
+++ convertJiraToList.py
@@ -14,58 +14,60 @@ def time_to_float(time_str: str) -> float:
 
 
 def seconds_to_timefloat(seconds: int) -> float:
-    time_dt_str = str(timedelta(seconds=seconds))
-    list_time_dt_str = time_dt_str.split(":")
+    time_dt_str = str(object=timedelta(seconds=seconds))
+    list_time_dt_str: list[str] = time_dt_str.split(sep=":")
     hour = int(list_time_dt_str[0])
     minute = int(list_time_dt_str[1])
-    fraction = minute / 60
-    result = round(hour + fraction, 4)
+    fraction: float = minute / 60
+    result: float = round(number=hour + fraction, ndigits=4)
     return result
+
+
+def determine_task(jitaclockwork: jira_clockwork) -> str:
+    if (
+        jitaclockwork.parent_summary.lower() == "all leaves"
+        and jitaclockwork.issue_type == "Activity"
+    ):
+        return "Leave"
+    elif (
+        any(
+            keyword in jitaclockwork.issue_summary.lower()
+            for keyword in [
+                "meeting",
+                "scrum activity",
+                "discussion",
+                "ประชุม",
+                "หารือ",
+            ]
+        )
+        or jitaclockwork.issue_type == "Activity"
+    ):
+        return "Other"
+    elif (
+        "consult / support" in jitaclockwork.parent_summary.lower()
+        or jitaclockwork.issue_summary.lower().startswith("support")
+    ):
+        return "Support"
+    return iat.default_task
+
+
+def determine_bill_type(jitaclockwork: jira_clockwork, task: str) -> str:
+    if task == "Leave" or (
+        jitaclockwork.issue_type == "Activity"
+        and jitaclockwork.issue_summary == "ATS Activity"
+    ):
+        return "Non-Billable"
+    elif (
+        jitaclockwork.comment.startswith("OT")
+        and jitaclockwork.issue_type == "Sub-task"
+    ):
+        return "Overtime"
+    return "Regular"
 
 
 def convert_jira_to_list(
     list_jitaclockwork: list[jira_clockwork],
 ) -> list[data_fill_jira]:
-    def determine_task(jitaclockwork: jira_clockwork) -> str:
-        if (
-            jitaclockwork.parent_summary.lower() == "all leaves"
-            and jitaclockwork.issue_type == "Activity"
-        ):
-            return "Leave"
-        elif (
-            any(
-                keyword in jitaclockwork.issue_summary.lower()
-                for keyword in [
-                    "meeting",
-                    "scrum activity",
-                    "discussion",
-                    "ประชุม",
-                    "หารือ",
-                ]
-            )
-            or jitaclockwork.issue_type == "Activity"
-        ):
-            return "Other"
-        elif (
-            "consult / support" in jitaclockwork.parent_summary.lower()
-            or jitaclockwork.issue_summary.lower().startswith("support")
-        ):
-            return "Support"
-        return iat.default_task
-
-    def determine_bill_type(jitaclockwork: jira_clockwork, task: str) -> str:
-        if task == "Leave" or (
-            jitaclockwork.issue_type == "Activity"
-            and jitaclockwork.issue_summary == "ATS Activity"
-        ):
-            return "Non-Billable"
-        elif (
-            jitaclockwork.comment.startswith("OT")
-            and jitaclockwork.issue_type == "Sub-task"
-        ):
-            return "Overtime"
-        return "Regular"
-
     data_fill_list: list[data_fill_jira] = []
 
     for jitaclockwork in list_jitaclockwork:
